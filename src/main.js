2cccb50d7b139682d5431edc133b9abeada97d41
amr <971157670@qq.com>
2019-06-19 08:44:02 +0800

    git commit -m change git commit -m

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import App from './App'
 import router from './router'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css' 
 Vue.config.productionTip = false
 fastClick.attach(document.body);
-/* eslint-disable no-new */
+Vue.use(VueAwesomeSwiper, /* { default global options } */)
+
 new Vue({
   el: '#app',
   router,
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
-    this.is Header
+    <div class="header">
+    <div class="header-left">
+        <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+        <span class="iconfont">&#xe632;</span>
+        输入城市/景点/游玩主题</div>
+    <div class="header-right">
+        城市
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div> 
     </div>
 </template>
 
@@ -10,6 +19,34 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus">
+ @import '~@/assets/varibles.styl' 
+  .header
+   display:flex 
+   line-height:0.86rem
+   background:$bgColor
+   color:#fff
+   .header-left
+    width:0.64rem
+    float:left
+    .back-icon
+     text-align:center
+     font-size:0.4rem
+   .header-input
+    flex:1
+    background:#fff
+    border-radius:0.1rem
+    margin-top:.12rem
+    height :.64rem
+    line-height :.64rem
+    margin-left:.2rem
+    color:#ccc
+    padding-left:0.2rem
+   .header-right
+    width:1.24rem
+    float:right
+    text-align:center
+    .arrow-icon
+     font-size .24rem
+     margin-left:-.04rem
 </style>
