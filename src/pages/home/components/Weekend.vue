a4da1835dd23a834d966ffca182a3d881d3a48e1
amr <971157670@qq.com>
2019-06-19 16:59:44 +0800

    index-finish

--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -4,7 +4,7 @@
         周末去哪儿
       </div>
       <ul>
-     <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+     <li class="item border-bottom" v-for="item in list" :key="item.id">
          <div class="item-img-wrapper">
              <img class="item-img" :src="item.imgUrl"/>
          </div>
@@ -20,25 +20,8 @@
 <script>
     export default {
         name:"HomeWeekend",
-        data(){
-            return{
-                recommendList:[{
-                    id:'0001',
-                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1712/91/a275569091681d02.jpg_640x200_0519ccb9.jpg',
-                    title:"大连圣亚海洋世界",
-                    desc:"浪漫大连首站，浪漫的海洋主题乐园"
-                },{
-                    id:'0002',
-                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1712/91/a275569091681d02.jpg_640x200_0519ccb9.jpg',
-                    title:"大连圣亚海洋世界",
-                    desc:"浪漫大连首站，浪漫的海洋主题乐园"
-                },{
-                    id:'0003',
-                    imgUrl:'http://img1.qunarzz.com/piao/fusion/1712/91/a275569091681d02.jpg_640x200_0519ccb9.jpg',
-                    title:"大连圣亚海洋世界",
-                    desc:"浪漫大连首站，浪漫的海洋主题乐园"
-                }]
-            }
+        props:{
+            list:Array
         }
     }
 </script>
@@ -46,14 +29,14 @@
 <style lang="stylus" scoped>
 @import '~@/assets/mixins.styl'
    .recommend-title
-     marign-top:.2rem
+     
      line-height:.8rem
      background:#eee
      text-indent:.2rem
    .item-img-wrapper
       overflow:hidden
       height:0
-      padding-bottom:33.9%
+      padding-bottom:37.09%
     .item-img
       width:100%
    .item-info
