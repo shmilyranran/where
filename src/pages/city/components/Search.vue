4adfb00a7c38e3a966f176b8b0da38a801ca2727
amr <971157670@qq.com>
2019-06-21 11:41:49 +0800

    city search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="search">
+     <input type="text" class="search-input" placeholder="输入城市名或拼音"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"CitySearch"
+    }
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/varibles.styl'
+.search
+ height:.72rem
+ background:$bgColor
+ padding:0 .1rem
+.search-input 
+    box-sizing:border-box
+    height:.62rem
+    list-height:.62rem
+    padding:0 .1rem
+    text-align:center
+    border-radius:.06rem
+    width:100%
+    color:#666
+</style>
